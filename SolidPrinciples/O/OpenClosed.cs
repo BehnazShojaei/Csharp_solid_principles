ff2deebb7d04bec1a627f71facaf332736721c67
Behnaz <behnaz.shojaei88@gmail.com>
2025-03-23 16:27:33 +0800

    notification system implemented to check open closed principle

---
+++ SolidPrinciples/O/OpenClosed.cs
@@ -0,0 +1,53 @@
+// software entities ... should be open for extension, but closed for modification
+// Importance
+// Extensibility: New features can be added without modifying existing code.
+// Stability: Reduces the risk of introducing bugs when making changes.
+// Flexibility: Adapts to changing requirements more easily.
+
+
+
+// build a simple notification system, initially for email later add sms (check notification type send the proper message)
+
+
+using System;
+
+namespace SolidPrinciples.O
+{
+    public interface Notification
+    {
+        void Send(string message);
+    }
+
+    public class EmailNotification : Notification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending EMAIL: {message}");
+        }
+    }
+
+    public class SmsNotification : Notification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending SMS: {message}");
+        }
+    }
+
+    public class OpenClosedExample
+    {
+        public static void Run()
+        {
+            var notifications = new Notification[]
+            {
+                new EmailNotification(),
+                new SmsNotification()
+            };
+
+            foreach (var notification in notifications)
+            {
+                notification.Send("System Alert: Something happened.");
+            }
+        }
+    }
+}
