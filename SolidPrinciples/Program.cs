6be91b320f0009bc880aaa06a3f51ec2007a46e1
Behnaz <behnaz.shojaei88@gmail.com>
2025-03-23 16:28:33 +0800

    updated run logic to be flexible for all principles

--- SolidPrinciples/Program.cs
+++ SolidPrinciples/Program.cs
@@ -2,11 +2,32 @@
 
 using System;
 using SolidPrinciples.S;
+using SolidPrinciples.O;
 
 class Program
 {
     static void Main(string[] args)
     {
-        SingleResponsibilityExample.Run();
+        Console.WriteLine("Select a SOLID Principle to run:");
+        Console.WriteLine("1. Single Responsibility");
+        Console.WriteLine("2. Open/Closed");
+        Console.WriteLine("Enter the number:");
+
+        string? input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                SingleResponsibilityExample.Run();
+                break;
+
+            case "2":
+                OpenClosedExample.Run();
+                break;
+
+            default:
+                Console.WriteLine("Invalid input. Exiting.");
+                break;
+        }
     }
 }
